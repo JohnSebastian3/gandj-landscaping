7c812d0fcb1980ffeaec7ef0e01675e07740a74a
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-07 17:31:32 -0500

    add config file for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        moss: "#56612A",
+        slate: "#e3e1df",
+        wood : "#4A442D",
+        sand: "#DCD6C8",
+        green: "#688B58",
+        "dark-green": "#25241a"
+      },
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        nanumgothic: ["Nanum Gothic", "sans-serif"],
+      },
+      content: {
+        landinghero: "url('./assets/hero.jpg')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1480px",
+    },
+  },
+  plugins: [],
+};
