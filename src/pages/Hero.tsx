330d1a223aa0860b68307ac51038a123b99c9d4e
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-07 17:32:56 -0500

    add components

---
+++ src/pages/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section
+      className={`bg-[url('./assets/hero3.jpg')] h-[100vh] bg-cover bg-center flex justify-center items-center`}
+    >
+      <header className="w-5/6 flex flex-col gap-4">
+        <h1 className="text-slate text-4xl font-bold">G & J Landscaping</h1>
+        <h2 className="text-slate text-2xl">Redefining outdoor experiences since 1989</h2>
+      </header>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/pages/Services.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section className="text-dark-green bg-sand">
+      <div className="w-5/6 mx-auto flex flex-col py-8 text-center">
+        <div className="flex flex-col gap-2 py-8 border-b border-dark-green border-opacity-10">
+          <h3>Service 1</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            id reiciendis cum odit deleniti perferendis rerum sit quibusdam! Hic
+            iusto vel, eaque corporis distinctio odit dicta incidunt atque est
+            deleniti?
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 py-8 border-b border-dark-green border-opacity-10">
+          <h3>Service 2</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            id reiciendis cum odit deleniti perferendis rerum sit quibusdam! Hic
+            iusto vel, eaque corporis distinctio odit dicta incidunt atque est
+            deleniti?
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 py-8 border-b border-dark-green border-opacity-10">
+          <h3>Service 3</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            id reiciendis cum odit deleniti perferendis rerum sit quibusdam! Hic
+            iusto vel, eaque corporis distinctio odit dicta incidunt atque est
+            deleniti?
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 py-8">
+          <h3>Service 4</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+            id reiciendis cum odit deleniti perferendis rerum sit quibusdam! Hic
+            iusto vel, eaque corporis distinctio odit dicta incidunt atque est
+            deleniti?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
