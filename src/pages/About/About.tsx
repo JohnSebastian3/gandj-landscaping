11c4c645182c795715f14448d6f04c6b8b598d7a
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-08 18:31:26 -0500

    add about component

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="text-dark-green">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
+      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
+      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
+      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
+      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
+      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
+      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
+    </div>
+  );
+};
+
+export default About;
