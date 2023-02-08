b717c7a3775c1940f0d8b9a0f44f1b619118c12d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-08 18:34:23 -0500

    add react router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import Navbar from "./pages/Navbar";
-import Hero from "./pages/Hero";
-import Main from "./pages/Main";
-import Services from "./pages/Services";
-import Testimonials from "./pages/Testimonials";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Landing from "./pages/Landing/Landing";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
+
   return (
-    <div className="font-playfair">
-      <Navbar />
-      <Hero />
-      <Main />
-      <Services />
-      <Testimonials />
-    </div>
+    <BrowserRouter>
+      <div className="font-playfair">
+        <Navbar
+        />
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/portfolio" element={<Portfolio />} ></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes> 
+      </div>
+    </BrowserRouter>
   );
 }
 
