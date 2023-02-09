7237da40e394e2ee3f6a293dda2cdbb1ad678451
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-09 14:58:39 -0500

    Rename directory from Landing to Home

---
+++ src/pages/Home/Content.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Intro from "./Intro";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+
+const Content = () => {
+  return (
+    <>
+      <Intro />
+      <Services />
+      <Testimonials />
+    </>
+  );
+};
+
+export default Content;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Hero from "./Hero";
+import Content from "./Content";
+
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+      <Content />
+    </>
+  );
+};
+
+export default Landing;
