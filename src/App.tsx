71a5cb1b095673819beab4568321b59399ae66a5
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-09 14:59:05 -0500

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
-import Landing from "./pages/Landing/Landing";
+import Landing from "./pages/Home/Home";
 import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} ></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes> 
+        <Footer />
       </div>
     </BrowserRouter>
   );
