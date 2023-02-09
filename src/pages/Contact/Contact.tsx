239e0fefff9ada8fc47e8eb6b13aa1c5f9aea3df
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-09 14:58:19 -0500

    Add content and header components to contact component

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import Content from './Content'
+import Header from './Header'
 
 const Contact = () => {
   return (
-    <div className="text-dark-green">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
-      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
-      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
-      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
-      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero
-      magni dolores quas nam asperiores quaerat incidunt adipisci. Ad hic quam
-      voluptas numquam illum adipisci odio quas repellendus delectus officiis!
-    </div>
+    <>
+      <Header />
+      <Content />
+    </>
   )
 }
 
