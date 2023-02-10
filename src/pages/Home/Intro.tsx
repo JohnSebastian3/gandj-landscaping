5e419127b06f2f4e7f8e08f3797987659eee3c84
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-09 20:46:49 -0500

    add paragraph blurb

--- src/pages/Home/Intro.tsx
+++ src/pages/Home/Intro.tsx
@@ -8,10 +8,24 @@ const Intro = () => {
           The best landscaping service in Long Island{" "}
         </h3>
         <p className="text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore fuga
-          enim inventore aliquid suscipit et iure, sunt cumque nihil quis!
-          Dolorum tempore officiis aliquid perspiciatis repellendus sit autem
-          illum placeat!
+          G & J Landscaping is a family-owned and operated landscaping
+          company that has been providing top-notch lawn care services to the
+          local community for over 20 years. Our team of experienced landscapers
+          are passionate about creating beautiful, thriving outdoor spaces for
+          our clients. We understand that every yard is
+          unique and we strive to provide personalized solutions to meet each of
+          our client's individual needs and preferences. Whether you're looking
+          to spruce up your front yard with some colorful flowers or transform
+          your backyard into a lush garden oasis, we've got you covered. We
+          pride ourselves on our attention to detail and commitment to customer
+          satisfaction. From the initial consultation to the final walk-through,
+          our team works closely with you to bring your landscaping vision to
+          life. We use only the highest quality materials and the latest
+          techniques to ensure that your lawn stays healthy and vibrant for
+          years to come. If you're looking for a reliable, professional, and
+          friendly landscaping company, look no further than G & J Landscaping.
+          Contact us today to schedule your consultation and start turning your
+          outdoor dreams into a reality!
         </p>
       </div>
     </section>
