96a1336881dfe0fd691e8cf07d98cbb5ec34b9b5
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-11 11:50:47 -0500

    add background color to header to darken image

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FiChevronDown} from 'react-icons/fi';
 
 const Hero = () => {
 
@@ -6,10 +7,19 @@ const Hero = () => {
     <section
       className={`bg-[url('./assets/hero3.jpg')] h-[100vh] bg-dark-green bg-opacity-80 bg-cover bg-center flex justify-center items-center bg-fixed`}
     >
-      <header className="w-5/6 md:w-2/3 flex flex-col gap-4">
-        <h1 className="text-slate text-4xl font-bold">G & J Landscaping</h1>
-        <h2 className="text-slate text-2xl">Redefining outdoor experiences since 1989</h2>
-      </header>
+        <div
+        className="bg-black bg-opacity-30 w-full h-full flex justify-center
+    items-center"
+      >
+        <header className="w-5/6 md:w-2/3 flex flex-col gap-4">
+          <h1 className="text-slate text-4xl font-bold">G & J Landscaping</h1>
+          <h2 className="text-slate text-2xl">Redifining outdoor experiences since blank</h2>
+          
+        </header>
+        <div className="absolute bottom-20">
+          <FiChevronDown className="text-4xl"/>
+        </div>
+      </div>
     </section>
   );
 };
