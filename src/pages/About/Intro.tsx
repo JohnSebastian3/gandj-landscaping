5d229313c7ec5d350c294368e83b935325572348
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-17 19:30:25 -0500

    add intro blurb to about

--- src/pages/About/Intro.tsx
+++ src/pages/About/Intro.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
-
 const Intro = () => {
   return (
-    <section className="text-dark-green py-12 w-5/6 md:w-2/3 mx-auto">
-    <h2>Our story</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aliquam
-      consequuntur omnis in dolorem beatae repudiandae nesciunt dignissimos
-      quisquam. Reprehenderit dolorum reiciendis totam animi consectetur
-      magnam ducimus quam doloremque maxime.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quidem
-      expedita explicabo consequatur pariatur, repellat repudiandae dolore.
-      Vitae, sint soluta dolores totam eius delectus, architecto animi nobis
-      error, quam placeat!
-    </p>
-  </section>
-  )
-}
+    <section className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto">
+      <div className="flex flex-col gap-4">
+        <h2>Our story</h2>
+        <p>
+          At G & J Landscaping, we believe that your outdoor space should be a
+          beautiful and functional extension of your home or business. Our team
+          is dedicated to designing, installing, and maintaining custom
+          landscapes that meet your unique needs and budget.
+        </p>
+        <p>
+          Whether you're looking for lawn care, garden design, tree services, or
+          outdoor lighting, we have the knowledge and expertise to help you
+          achieve your vision. We use only the highest quality materials and
+          equipment to ensure that your landscaping looks great and lasts for
+          years to come.
+        </p>
+        <p>
+          Contact us today to schedule a consultation and see how we can
+          transform your outdoor space into a beautiful and functional oasis.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
