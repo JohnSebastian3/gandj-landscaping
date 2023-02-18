ed7ae65e31a5a82079327c01b2cfbba3bc95c713
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-17 19:30:07 -0500

    add email contact

--- src/pages/Contact/Content.tsx
+++ src/pages/Contact/Content.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Content = () => {
   return (
-    <div className="text-dark-green py-12 w-5/6 md:w-2/3 mx-auto">
-      Ready to upgrade your outdoor space? Call us at (516) 949-1482 or email us
-      at -------@gmail.com
+    <div className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto">
+      <p>
+        Ready to upgrade your outdoor space? Call us at (516) 949-1482 or email
+        us at jg4341751@gmail.com
+      </p>
     </div>
   );
 };
