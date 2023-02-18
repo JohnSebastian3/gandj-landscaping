0d9082742199d02ca6bfae2bef474bb24db4aaab
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-17 19:28:16 -0500

    add additional info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className='bg-light-gray text-dark-green'>
-      <div className='w-5/6 mx-auto py-8'>
-        <div className='flex flex-col items-center gap-2'>
-        <h2>LOGO</h2>
-        <span>Phone number</span>
-        <span>Email</span>
-        <small>Copyright &copy; 2023 G & J Landscaping</small>
-
+    <footer className="bg-light-gray text-dark-green">
+      <div className="w-5/6 mx-auto py-8">
+        <div className="flex flex-col items-center gap-2">
+          <h2>LOGO</h2>
+          <div className="flex items-center gap-2">
+            <BsFillTelephoneFill />
+            <span>(516) 949-1482</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <FiMail />
+            <span>jg4341751@gmail.com</span>
+          </div>
+          <small>Copyright &copy; 2023 G & J Landscaping</small>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
