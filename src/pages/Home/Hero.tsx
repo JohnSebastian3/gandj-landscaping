d84a76b1d47550d206542d9c73c04eb602d57b35
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-17 19:29:49 -0500

    add react AnchorLink for smooth scrolling

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import {FiChevronDown} from 'react-icons/fi';
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { FiChevronDown } from "react-icons/fi";
 
 const Hero = () => {
-
   return (
     <section
       className={`bg-[url('./assets/hero3.jpg')] h-[100vh] bg-dark-green bg-opacity-80 bg-cover bg-center flex justify-center items-center bg-fixed`}
     >
-        <div
+      <div
         className="bg-black bg-opacity-30 w-full h-full flex justify-center
     items-center"
       >
         <header className="w-5/6 md:w-2/3 flex flex-col gap-4">
           <h1 className="text-slate text-4xl font-bold">G & J Landscaping</h1>
-          <h2 className="text-slate text-2xl">Redifining outdoor experiences since blank</h2>
-          
+          <h2 className="text-slate text-2xl">
+            Redifining outdoor experiences since 1994
+          </h2>
         </header>
         <div className="absolute bottom-20">
-          <FiChevronDown className="text-4xl"/>
+          <AnchorLink href="#intro">
+            <FiChevronDown className="text-4xl" />
+          </AnchorLink>
         </div>
       </div>
     </section>
