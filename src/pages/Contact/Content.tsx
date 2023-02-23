fa8d7a6e1d07b6a9e36c264d1d7bffd6958f8f7d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-23 14:13:06 -0500

    add div with contact info and icons

--- src/pages/Contact/Content.tsx
+++ src/pages/Contact/Content.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 
 const Content = () => {
   return (
-    <div className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto text-center">
-      <p>Ready to upgrade your outdoor space?</p>
-      <p>Call us at (516) 949-1482 or email us at jg4341751@gmail.com</p>
+    <div className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto text-center flex flex-col gap-8">
+      <p className="text-2xl mx:text-4xl">
+        Ready to upgrade your outdoor space?
+      </p>
 
       <p>
         We are located in Oceanside, New York and service houses and clients in
         Long Island around the 5 towns areas.
       </p>
+
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex items-center gap-2">
+          <BsFillTelephoneFill />
+          <span>(516) 949-1482</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <FiMail />
+          <span>jg4341751@gmail.com</span>
+        </div>
+      </div>
       <div className="w-full">
         <iframe
           width="100%"
