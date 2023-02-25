e6fbba54ee104407964d927312ea43fe1cedcc10
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-25 14:14:07 -0500

    add images to grid

--- src/pages/Portfolio/Content.tsx
+++ src/pages/Portfolio/Content.tsx
@@ -1,8 +1,44 @@
 const Content = () => {
   return (
-    <section className="w-5/6 md:w-2/3 py-24 mx-auto">
+    <section className="w-5/6 md:w-2/3 py-24 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
       <div
-        className={`bg-[url('./assets/img1.jpg')] h-[300px] w-[300px] bg-cover bg-center`}
+        className={`bg-[url('./assets/img1.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img2.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img3.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      {/* <div
+        className={`bg-[url('./assets/img4.jpg')] h-[300px] bg-cover bg-center`}
+      ></div> */}
+      {/* <div
+        className={`bg-[url('./assets/img5.jpg')] h-[300px] bg-cover bg-center`}
+      ></div> */}
+      <div
+        className={`bg-[url('./assets/img6.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      {/* <div
+        className={`bg-[url('./assets/img7.jpg')] h-[300px] bg-cover bg-center`}
+      ></div> */}
+      <div
+        className={`bg-[url('./assets/img8.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img9.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img10.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img11.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img12.jpg')] h-[300px] bg-cover bg-center`}
+      ></div>
+      <div
+        className={`bg-[url('./assets/img13.jpg')] h-[300px] bg-cover bg-center`}
       ></div>
     </section>
   );
