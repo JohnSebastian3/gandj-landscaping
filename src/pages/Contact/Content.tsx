486c0f928489b27ab3594e66c12e8d9be38bde39
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-02-28 14:04:59 -0500

    add google maps iframe to contact page

--- src/pages/Contact/Content.tsx
+++ src/pages/Contact/Content.tsx
@@ -2,11 +2,29 @@ import React from "react";
 
 const Content = () => {
   return (
-    <div className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto">
+    <div className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto text-center">
+      <p>Ready to upgrade your outdoor space?</p>
+      <p>Call us at (516) 949-1482 or email us at jg4341751@gmail.com</p>
+
       <p>
-        Ready to upgrade your outdoor space? Call us at (516) 949-1482 or email
-        us at jg4341751@gmail.com
+        We are located in Oceanside, New York and service houses and clients in
+        Long Island around the 5 towns areas.
       </p>
+      <div className="w-full">
+        <iframe
+          width="100%"
+          height="600"
+          frameBorder="0"
+          loading="lazy"
+          title="G & J Landscaping"
+          scrolling="no"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Oceanside,%20New%20York+(My%20Business%20Name)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.maps.ie/distance-area-calculator.html">
+            area maps
+          </a>
+        </iframe>
+      </div>
     </div>
   );
 };
