f8166b5ecc68ba05e5ba8a5daa1609d124eb2ec5
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-03-01 16:35:14 -0500

    add Link component from react router

--- src/pages/About/Intro.tsx
+++ src/pages/About/Intro.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Intro = () => {
   return (
     <section className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto">
-      <div className="flex flex-col gap-4 text-center">
+      <div className="flex flex-col gap-4 text-center md:gap-12">
         <h2 className="text-2xl md:text-4xl">Our story</h2>
         <p>
           At G & J Landscaping, we believe that your outdoor space should be a
@@ -16,9 +18,15 @@ const Intro = () => {
           equipment to ensure that your landscaping looks great and lasts for
           years to come.
         </p>
-        <p>
-          Contact us today to schedule a consultation and see how we can
-          transform your outdoor space into a beautiful and functional oasis.
+        <p className="">
+          <Link
+            to={"/contact"}
+            className="text-green hover:border-b-2 hover:border-b-green transition duration-200"
+          >
+            Contact us
+          </Link>{" "}
+          today to schedule a consultation and see how we can transform your
+          outdoor space into a beautiful and functional oasis.
         </p>
       </div>
     </section>
--- src/pages/Home/Intro.tsx
+++ src/pages/Home/Intro.tsx
@@ -1,21 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Intro = () => {
   return (
     <section
       id="intro"
       className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto"
     >
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 md:gap-12">
         <h3 className="text-center text-2xl md:text-4xl">
           The best landscaping service in Long Island{" "}
         </h3>
-        <p className="text-center">
+        <p className="text-center md:text-left">
           G & J Landscaping is a family-owned and operated landscaping company
           that has been providing top-notch lawn care services to the local
           community in Long Island, NY for over 25 years. Our team of
           experienced landscapers are passionate about creating beautiful,
-          thriving outdoor spaces for our clients. Contact us today to schedule
-          your consultation and start turning your outdoor dreams into a
-          reality!
+          thriving outdoor spaces for our clients.{" "}
+          <Link
+            to={"/contact"}
+            className="text-green hover:border-b-2 hover:border-b-green transition duration-200"
+          >
+            Contact us
+          </Link>{" "}
+          today to schedule your consultation and start turning your outdoor
+          dreams into a reality!
           <p></p>
         </p>
       </div>
