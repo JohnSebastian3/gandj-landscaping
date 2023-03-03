b9ed4f833f9522119e80e2b37f7ea300ea57c91b
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-03-03 14:48:16 -0500

    Add framer motion to page headers

--- src/pages/About/Header.tsx
+++ src/pages/About/Header.tsx
@@ -1,22 +1,52 @@
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
-    <section
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.1 }}
+      transition={{ duration: 0.6 }}
+      variants={{
+        hidden: { opacity: 0.4 },
+        visible: { opacity: 1 },
+      }}
       className={`bg-[url('./assets/img5.jpg')] h-[75vh] bg-cover bg-center flex justify-center items-center bg-fixed`}
     >
       <div
-        className="bg-black bg-opacity-40 w-full h-full flex justify-center
+        className="bg-black bg-opacity-50 w-full h-full flex justify-center
       items-center"
       >
         <header className="w-5/6 md:w-2/3 flex flex-col gap-4">
-          <h1 className="text-slate text-4xl font-bold md:text-6xl">
+          <motion.h1
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.1 }}
+            transition={{ duration: 1, delay: 0.6 }}
+            variants={{
+              hidden: { opacity: 0, x: -60 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="text-slate text-4xl font-bold md:text-6xl"
+          >
             About Us
-          </h1>
-          <h2 className="text-slate text-2xl md:text-3xl">
+          </motion.h1>
+          <motion.h2
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 1, delay: 1 }}
+            variants={{
+              hidden: { opacity: 0, x: -60 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="text-slate text-2xl md:text-3xl"
+          >
             We are a team of experienced and passionate professionals
-          </h2>
+          </motion.h2>
         </header>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,22 +1,51 @@
+import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { FiChevronDown } from "react-icons/fi";
 
 const Hero = () => {
   return (
-    <section
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.1 }}
+      transition={{ duration: 0.8 }}
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      }}
       className={`bg-[url('./assets/img2.jpg')] h-[100vh] bg-dark-green bg-opacity-80 bg-cover bg-center flex justify-center items-center bg-fixed`}
     >
       <div
-        className="bg-black bg-opacity-30 w-full h-full flex justify-center
+        className="bg-black bg-opacity-50 w-full h-full flex justify-center
     items-center"
       >
         <header className="w-5/6 md:w-2/3 flex flex-col gap-4">
-          <h1 className="text-slate text-4xl font-bold md:text-6xl">
+          <motion.h1
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.1 }}
+            transition={{ duration: 1.8, delay: 0.6 }}
+            variants={{
+              hidden: { opacity: 0, x: -60 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="text-slate text-4xl font-bold md:text-6xl"
+          >
             G & J Landscaping
-          </h1>
-          <h2 className="text-slate text-2xl md:text-3xl">
+          </motion.h1>
+          <motion.h2
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 1.8, delay: 1 }}
+            variants={{
+              hidden: { opacity: 0, x: -60 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            className="text-slate text-2xl md:text-3xl"
+          >
             Redifining outdoor experiences since 1994
-          </h2>
+          </motion.h2>
         </header>
         <div className="absolute bottom-20">
           <AnchorLink href="#intro">
@@ -24,7 +53,7 @@ const Hero = () => {
           </AnchorLink>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
