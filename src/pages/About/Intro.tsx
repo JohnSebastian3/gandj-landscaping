14ab919bdbbc519a223ad24b63f3ecc572dd536d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-03-06 16:19:13 -0500

    add padding for medium screens and framer motion

--- src/pages/About/Intro.tsx
+++ src/pages/About/Intro.tsx
@@ -1,24 +1,65 @@
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 const Intro = () => {
   return (
-    <section className="text-dark-green py-24 w-5/6 md:w-2/3 mx-auto">
-      <div className="flex flex-col gap-4 text-center md:gap-12">
-        <h2 className="text-2xl md:text-4xl">Our story</h2>
-        <p>
+    <section className="text-dark-green py-24 md:py-48 w-5/6 md:w-2/3 mx-auto">
+      <div className="flex flex-col gap-4 text-center md:gap-12 md:text-left">
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.8 }}
+          transition={{ duration: 1 }}
+          variants={{
+            hidden: { opacity: 0, y: -60 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="text-2xl md:text-4xl text-center"
+        >
+          Our story
+        </motion.h2>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1, delay: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, x: -60 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           At G & J Landscaping, we believe that your outdoor space should be a
           beautiful and functional extension of your home or business. Our team
           is dedicated to designing, installing, and maintaining custom
           landscapes that meet your unique needs and budget.
-        </p>
-        <p>
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1, delay: 0.8 }}
+          variants={{
+            hidden: { opacity: 0, x: -60 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           Whether you're looking for lawn care, garden design, tree services, or
           outdoor lighting, we have the knowledge and expertise to help you
           achieve your vision. We use only the highest quality materials and
           equipment to ensure that your landscaping looks great and lasts for
           years to come.
-        </p>
-        <p className="">
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 1, delay: 1 }}
+          variants={{
+            hidden: { opacity: 0, x: -60 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className=""
+        >
           <Link
             to={"/contact"}
             className="text-green hover:border-b-2 hover:border-b-green transition duration-200"
@@ -27,7 +68,7 @@ const Intro = () => {
           </Link>{" "}
           today to schedule a consultation and see how we can transform your
           outdoor space into a beautiful and functional oasis.
-        </p>
+        </motion.p>
       </div>
     </section>
   );
--- src/pages/About/Qualities.tsx
+++ src/pages/About/Qualities.tsx
@@ -2,15 +2,36 @@ import { TfiIdBadge } from "react-icons/tfi";
 import { SlBadge } from "react-icons/sl";
 import { IoIosTimer } from "react-icons/io";
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
+import { motion } from "framer-motion";
 
 const Qualities = () => {
   return (
     <section className="text-slate bg-green">
-      <h3 className="text-center text-2xl md:text-4xl pt-24">
+      <motion.h3
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.9 }}
+        transition={{ duration: 1 }}
+        variants={{
+          hidden: { opacity: 0, y: -40 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        className="text-center text-2xl md:text-4xl pt-24 md:pt-36"
+      >
         How we stand out
-      </h3>
-      <div className="w-5/6 md:w-2/3 mx-auto flex flex-col sm:grid sm:grid-rows-2 sm:grid-cols-2 pt-12 pb-24  text-center">
-        <div className="flex flex-col gap-2 py-8 border-b sm:border-r sm:px-4 border-slate border-opacity-10">
+      </motion.h3>
+      <div className="w-5/6 md:w-2/3 mx-auto flex flex-col sm:grid sm:grid-rows-2 sm:grid-cols-2 pt-12 pb-24 md:pb-36 md:pt-24 text-center">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex flex-col gap-2 py-8 border-b sm:border-r sm:px-4 border-slate border-opacity-10"
+        >
           <div className="flex flex-col gap-4 items-center">
             <SlBadge className="text-4xl" />
             <h3>Experience & Expertise</h3>
@@ -19,8 +40,18 @@ const Qualities = () => {
             We have a team of experienced professionals with extensive knowledge
             and skills in all aspects of landscaping.
           </p>
-        </div>
-        <div className="flex flex-col gap-2 py-8 border-b sm:px-4 border-slate border-opacity-10">
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+          transition={{ duration: 1, delay: 0.4 }}
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex flex-col gap-2 py-8 border-b sm:px-4 border-slate border-opacity-10"
+        >
           <div className="flex flex-col gap-4 items-center">
             <TfiIdBadge className="text-4xl" />
             <h3>Professionalism</h3>
@@ -30,8 +61,18 @@ const Qualities = () => {
             response to inquiries, clear and concise contracts, and ethical
             practices.
           </p>
-        </div>
-        <div className="flex flex-col gap-2 py-8 border-b sm:border-r sm:px-4 sm:border-b-0 border-slate border-opacity-10">
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+          transition={{ duration: 1, delay: 0.6 }}
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex flex-col gap-2 py-8 border-b sm:border-r sm:px-4 sm:border-b-0 border-slate border-opacity-10"
+        >
           <div className="flex flex-col gap-4 items-center">
             <IoIosTimer className="text-4xl" />
             <h3>Timely Delivery</h3>
@@ -40,8 +81,18 @@ const Qualities = () => {
             We complete our projects within the agreed time frame, ensuring that
             it does not cause any inconvenience to you.
           </p>
-        </div>
-        <div className="flex flex-col gap-2 sm:px-4 py-8">
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+          transition={{ duration: 1, delay: 0.8 }}
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          className="flex flex-col gap-2 sm:px-4 py-8"
+        >
           <div className="flex flex-col gap-4 items-center">
             <HiOutlineWrenchScrewdriver className="text-4xl" />
             <h3>Ongoing Maintenance</h3>
@@ -50,7 +101,7 @@ const Qualities = () => {
             We provide ongoing maintenance services to ensure that the landscape
             remains healthy and beautiful for years to come.
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
